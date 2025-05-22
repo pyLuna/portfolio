56d81e2b3b0b48afcb91794ac9a158e0590d07fe
pyLuna <john.joshua.demartin.celis@gmail.com>
2025-05-21 21:21:59 -0900

    logging mongo initialization

--- next/src/server/mongo/db.init.ts
+++ next/src/server/mongo/db.init.ts
@@ -1,10 +1,12 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
+const uri = `mongodb+srv://${process.env.NEXT_DB_USER}:${process.env.NEXT_DB_PASSWORD}@main.nttg7ye.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=main`;
 
-const uri = `mongodb+srv://${process.env.NEXT_DB_USER}:${process.env.NEXT_DB_PASSWORD}@main.nttg7ye.mongodb.net/?retryWrites=true&w=majority&appName=main`;
+// const uri = `mongodb+srv://${process.env.NEXT_DB_USER}:${process.env.NEXT_DB_PASSWORD}@main.nttg7ye.mongodb.net/?retryWrites=true&w=majority&appName=main`;
 
 let cachedClient: MongoClient | null = null;
 
 const getMongoClient = async (): Promise<MongoClient> => {
+    console.log("uri", uri, "user": process.env.NEXT_DB_USER, "pass": process.env.NEXT_DB_PASSWORD);
     if (cachedClient) {
         return cachedClient;
     }
