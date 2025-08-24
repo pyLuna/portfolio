bf4ad0728b48eb67b8268dea783b0fadae932d5c
pyLuna <john.joshua.demartin.celis@gmail.com>
2025-08-24 23:13:32 +0800

    httpOnly

--- next/src/app/api/admin/login/route.ts
+++ next/src/app/api/admin/login/route.ts
@@ -6,32 +6,38 @@ import { generateToken } from "@/server/token/token";
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
-    const response = await req.json();
+  const response = await req.json();
 
-    const user = await getUserByUsername(response?.username);
+  const user = await getUserByUsername(response?.username);
 
-    if (!user?.password) {
-        return error({ "message": "User not found." }, { status: 404, string_code: ErrorCode.user_not_found });
-    }
+  if (!user?.password) {
+    return error(
+      { message: "User not found." },
+      { status: 404, string_code: ErrorCode.user_not_found }
+    );
+  }
 
-    const isPasswordValid = await compare(response?.password, user?.password!);
+  const isPasswordValid = await compare(response?.password, user?.password!);
 
-    if (!isPasswordValid) {
-        return error({ "message": "Invalid password." }, { status: 400, string_code: ErrorCode.invalid_password });
-    }
+  if (!isPasswordValid) {
+    return error(
+      { message: "Invalid password." },
+      { status: 400, string_code: ErrorCode.invalid_password }
+    );
+  }
 
-    delete user.password;
+  delete user.password;
 
-    const token = generateToken(user);
+  const token = generateToken(user);
 
-    const c = await cookies();
+  const c = await cookies();
 
-    c.set("token", token, {
-        maxAge: 8 * 1000 * 60,
-        path: "/",
-        secure: process.env.NODE_ENV === "production",
-    });
+  c.set("token", token, {
+    maxAge: 8 * 1000 * 60,
+    path: "/",
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  });
 
-    return json({ token, ...user });
+  return json({ token, ...user });
 }
-
